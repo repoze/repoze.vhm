0839c1a098050257393d487c2a820952f3a8b29f
Nathan Van Gheem <vangheem@gmail.com>
2010-01-01 07:53:06 +0000

    added 0.12 release and added CHANGES.txt to long_description of package

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(name='repoze.vhm',
       version=__version__,
       description='repoze virtual hosting middleware.',
-      long_description=README,
+      long_description=README + CHANGES,
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
